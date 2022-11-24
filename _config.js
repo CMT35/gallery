2380e2d80696e2b98d876a8e7dce30b67b758477
Chris Thuo <christhuoo96@gmail.com>
2022-11-24 12:09:35 +0300

    updating config file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://CMT8075:testing1234@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://CMT8075:testing1234@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://CMT8075:testing1234@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://CMT8075:test1234@first-ip1.j2hfk3i.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://CMT8075:test1234@first-ip1.j2hfk3i.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://CMT8075:test1234@first-ip1.j2hfk3i.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
